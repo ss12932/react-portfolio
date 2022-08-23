73c45abc36d288a34c9bf53ead28abb423d1ba8a
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-23 13:42:11 +0100

    created initial mobile menu overlay

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assets/logo.png';
 
@@ -9,27 +10,25 @@ function NavBar() {
       </div>
 
       {/* menu */}
-      <div>
-        <ul className="flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-
-      {/* hamburger */}
-      <div className="hidden">
-        <FaBars />
-      </div>
-      {/* mobile menu */}
-      <ul className="hidden">
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
         <li>Work</li>
         <li>Contact</li>
+      </ul>
+
+      {/* hamburger */}
+      <div className="md:hidden z-10">
+        <FaBars />
+      </div>
+      {/* mobile menu */}
+      <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
       {/* social icons */}
       <div className="hidden"></div>
