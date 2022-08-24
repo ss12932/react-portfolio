cc059f59078e309e8969db761824990ff0c7dd74
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-24 22:17:40 +0100

    update project cards with react icons

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,11 @@
-import PetMVP from '../assets/mvppet.png';
+import { FaGithub, FaCode } from 'react-icons/fa';
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div
+      name="work"
+      className="w-full md:min-h-screen text-gray-300 bg-[#0a192f]"
+    >
       <div className="max-w-7xl mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
@@ -26,8 +29,12 @@ const Work = () => {
                 with disabilities and also to optimise SEO.
               </p>
               <div className="mx-auto space-x-6">
-                <button className="btn bg-[#e31a6d] mx-auto">Demo</button>
-                <button className="btn bg-[#e31a6d] mx-auto">Code</button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
               </div>
             </div>
           </div>
@@ -46,8 +53,12 @@ const Work = () => {
                 CSS and vanilla Javascript.
               </p>
               <div className="mx-auto space-x-6">
-                <button className="btn bg-[#e31a6d] mx-auto">Demo</button>
-                <button className="btn bg-[#e31a6d] mx-auto">Code</button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
               </div>
             </div>
           </div>
@@ -66,8 +77,12 @@ const Work = () => {
                 password based on criteria that they've selected.
               </p>
               <div className="mx-auto space-x-6">
-                <button className="btn bg-[#e31a6d] mx-auto">Demo</button>
-                <button className="btn bg-[#e31a6d] mx-auto">Code</button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
               </div>
             </div>
           </div>
@@ -88,8 +103,12 @@ const Work = () => {
                 feature a highscore section to gauge progress with fellow peers.
               </p>
               <div className="mx-auto space-x-6">
-                <button className="btn bg-[#e31a6d] mx-auto">Demo</button>
-                <button className="btn bg-[#e31a6d] mx-auto">Code</button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
               </div>
             </div>
           </div>
@@ -110,8 +129,12 @@ const Work = () => {
                 jQuery.
               </p>
               <div className="mx-auto space-x-6">
-                <button className="btn bg-[#e31a6d] mx-auto">Demo</button>
-                <button className="btn bg-[#e31a6d] mx-auto">Code</button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
               </div>
             </div>
           </div>
@@ -132,8 +155,215 @@ const Work = () => {
                 storage to store persistent data.
               </p>
               <div className="mx-auto space-x-6">
-                <button className="btn bg-[#e31a6d] mx-auto">Demo</button>
-                <button className="btn bg-[#e31a6d] mx-auto">Code</button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card max-w-sm bg-base-100 shadow-xl rounded-none mx-auto">
+            <figure className="h-52 overflow-hidden">
+              <img
+                className="w-full h-full object-cover object-top"
+                src="https://github.com/ss12932/weather-dashboard/raw/main/assets/img/desktop-view.png"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body bg-blue-900 space-y-2">
+              <h2 className="card-title font-bold">Weather Dashboard</h2>
+              <p className="text-sm">
+                A weather dashboard that will run in the browser and feature
+                dynamically with updated HTML and CSS. Using the Openweather one
+                call API to retrieve weather data for cities and use local
+                storage to store persistent data.
+              </p>
+              <div className="mx-auto space-x-6">
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card max-w-sm bg-base-100 shadow-xl rounded-none mx-auto">
+            <figure className="h-52 overflow-hidden">
+              <img
+                className="w-full h-full object-cover object-top"
+                src="https://github.com/ss12932/currency-news-project/raw/main/assets/images/app/home-page.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body bg-blue-900 space-y-2">
+              <h2 className="card-title font-bold">
+                MoneyMail - Currency News Project
+              </h2>
+              <p className="text-sm">
+                An app that allows you to exchange currencies with a click of a
+                button. The chosen currencies will display news articles,
+                informing users of current news surrounding the currency.
+              </p>
+              <div className="mx-auto space-x-6">
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card max-w-sm bg-base-100 shadow-xl rounded-none mx-auto">
+            <figure className="h-52 overflow-hidden">
+              <img
+                className="w-full h-full object-cover object-top"
+                src="https://github.com/ss12932/readme-generator/raw/main/public/assets/images/screenshot.png"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body bg-blue-900 space-y-2">
+              <h2 className="card-title font-bold">Readme Generator</h2>
+              <p className="text-sm">
+                A command-line application that dynamically generates a
+                professional README.md file from a user's input using the
+                Inquirer package.
+              </p>
+              <div className="mx-auto space-x-6">
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card max-w-sm bg-base-100 shadow-xl rounded-none mx-auto">
+            <figure className="h-52 overflow-hidden">
+              <img
+                className="w-full h-full object-cover object-top"
+                src="https://github.com/ss12932/team-profile-generator/raw/main/public/assets/img/team-profile.png"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body bg-blue-900 space-y-2">
+              <h2 className="card-title font-bold">Team Profile Generator</h2>
+              <p className="text-sm">
+                A Node.js CLI application that takes in information about
+                employees on a software engineering team , then generates a HTML
+                webpage that displays summaries for each person. This
+                application will be following TDD principles using jest to make
+                code more maintainable.
+              </p>
+              <div className="mx-auto space-x-6">
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card max-w-sm bg-base-100 shadow-xl rounded-none mx-auto">
+            <figure className="h-52 overflow-hidden">
+              <img
+                className="w-full h-full object-cover object-top"
+                src="https://github.com/ss12932/note-taker/raw/main/public/assets/images/11-express-homework-demo-02.png"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body bg-blue-900 space-y-2">
+              <h2 className="card-title font-bold">Note Taker</h2>
+              <p className="text-sm">
+                An application that can be used to write and save notes. This
+                application will use an Express.js back end and will save and
+                retrieve note data from a JSON file.
+              </p>
+              <div className="mx-auto space-x-6">
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card max-w-sm bg-base-100 shadow-xl rounded-none mx-auto">
+            <figure className="h-52 overflow-hidden">
+              <img
+                className="w-full h-full object-cover object-top"
+                src="https://github.com/ss12932/employee-tracker/raw/main/public/assets/images/cms_screenshot.png"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body bg-blue-900 space-y-2">
+              <h2 className="card-title font-bold">Employee Tracker</h2>
+              <p className="text-sm">
+                A command-line application that manages a company's employee
+                database, using Node.js, Inquirer, and MySQL.
+              </p>
+              <div className="mx-auto space-x-6">
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card max-w-sm bg-base-100 shadow-xl rounded-none mx-auto">
+            <figure className="h-52 overflow-hidden">
+              <img
+                className="w-full h-full object-cover object-top"
+                src="https://github.com/ss12932/employee-tracker/raw/main/public/assets/images/cms_screenshot.png"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body bg-blue-900 space-y-2">
+              <h2 className="card-title font-bold">Employee Tracker</h2>
+              <p className="text-sm">
+                A command-line application that manages a company's employee
+                database, using Node.js, Inquirer, and MySQL.
+              </p>
+              <div className="mx-auto space-x-6">
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card max-w-sm bg-base-100 shadow-xl rounded-none mx-auto">
+            <figure className="h-52 overflow-hidden">
+              <img
+                className="w-full h-full object-cover object-top"
+                src="https://github.com/ss12932/mvp-pet-app/raw/main/public/images/UI1.png"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body bg-blue-900 space-y-2">
+              <h2 className="card-title font-bold">My Valuable Pet</h2>
+              <p className="text-sm">
+                This full-stack application has been designed with pet owners in
+                mind, giving the user the ability to keep track of all their
+                pets needs and data.
+              </p>
+              <div className="mx-auto space-x-6">
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaGithub />
+                </button>
+                <button className="btn bg-[#e31a6d] mx-auto">
+                  <FaCode />
+                </button>
               </div>
             </div>
           </div>
