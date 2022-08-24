94f2bca21d3b5cd98923da1b6e76b90b31781791
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-24 14:47:27 +0100

    added avatar to home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,28 +4,40 @@ function Home() {
   return (
     <div name="home" className="bg-[#0a192f] w-full h-screen">
       {/* container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-pink-600">Hi, My name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Sinh Fu San
-        </h1>
-        <h2 className="text-4xl sm:text:text-7xl font-bold text-[#8892b0]">
-          I'm a full stack web developer
-        </h2>
-        <p className="text-[#8892b0] py-4 max-w-[500px]">
-          I'm a full stack developer specializing in the MERN stack. Currently,
-          I'm focused on building responsive full stack applications.
-        </p>
+      <div className="container flex flex-col lg:flex-row items-center justify-center h-full mx-auto space-y-20 lg:space-x-20">
+        <div className="max-w-[1000px] px-8 flex flex-col justify-center lg:h-full">
+          <p className="text-pink-600">Hi, My name is</p>
+          <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+            Sinh Fu San
+          </h1>
+          <h2 className="text-4xl sm:text:text-7xl font-bold text-[#8892b0]">
+            I'm a full stack web developer
+          </h2>
+          <p className="text-[#8892b0] py-4 max-w-[500px]">
+            I'm a full stack developer specializing in the MERN stack.
+            Currently, I'm focused on building responsive full stack
+            applications.
+          </p>
+          <div>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center mx-auto lg:mx-0 hover:bg-pink-600 hover:border-pink-600">
+              View Work
+              <span className="block group-hover:hidden">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+              <span className="hidden group-hover:block">
+                <HiArrowNarrowDown className="ml-3" />
+              </span>
+            </button>
+          </div>
+        </div>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            View Work
-            <span className="block group-hover:hidden">
-              <HiArrowNarrowRight className="ml-3" />
-            </span>
-            <span className="hidden group-hover:block">
-              <HiArrowNarrowDown className="ml-3" />
-            </span>
-          </button>
+          <div>
+            <img
+              className="w-64 h-64"
+              src="https://avatars.githubusercontent.com/u/97243089?v=4"
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </div>
