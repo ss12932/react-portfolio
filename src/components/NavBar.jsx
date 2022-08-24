1749ef49aaac8c5f0627d7253f062813af43fbb6
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-24 22:24:50 +0100

    fixed bug - content cutting navbar when scrolling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,7 +14,7 @@ function NavBar() {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-10">
       <div>
         <img src={Logo} alt="logo" style={{ width: '50px' }} />
       </div>
