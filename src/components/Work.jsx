643e3862e193f9f37852c4eab86adc27250697d9
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-24 17:57:42 +0100

    dummy proejct cards created for work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,24 +1,91 @@
+import PetMVP from '../assets/mvppet.png';
+
 const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
-      <div className="ma-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-7xl mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
           </p>
           <p className="py-6">Check out my recent work</p>
         </div>
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <div className="">
-            <div>
-              <span></span>
-              <div>
-                <a href="/">
-                  <button></button>
-                </a>
-                <a href="/">
-                  <button></button>
-                </a>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div class="card max-w-sm bg-base-100 shadow-xl mx-auto">
+            <figure>
+              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="mx-auto space-x-6">
+                <button class="btn btn-primary mx-auto">Demo</button>
+                <button class="btn btn-primary mx-auto">Code</button>
+              </div>
+            </div>
+          </div>
+          <div class="card max-w-sm bg-base-100 shadow-xl mx-auto">
+            <figure>
+              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="mx-auto space-x-6">
+                <button class="btn btn-primary mx-auto">Demo</button>
+                <button class="btn btn-primary mx-auto">Code</button>
+              </div>
+            </div>
+          </div>
+          <div class="card max-w-sm bg-base-100 shadow-xl mx-auto">
+            <figure>
+              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="mx-auto space-x-6">
+                <button class="btn btn-primary mx-auto">Demo</button>
+                <button class="btn btn-primary mx-auto">Code</button>
+              </div>
+            </div>
+          </div>
+          <div class="card max-w-sm bg-base-100 shadow-xl mx-auto">
+            <figure>
+              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="mx-auto space-x-6">
+                <button class="btn btn-primary mx-auto">Demo</button>
+                <button class="btn btn-primary mx-auto">Code</button>
+              </div>
+            </div>
+          </div>
+          <div class="card max-w-sm bg-base-100 shadow-xl mx-auto">
+            <figure>
+              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="mx-auto space-x-6">
+                <button class="btn btn-primary mx-auto">Demo</button>
+                <button class="btn btn-primary mx-auto">Code</button>
+              </div>
+            </div>
+          </div>
+          <div class="card max-w-sm bg-base-100 shadow-xl mx-auto">
+            <figure>
+              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="mx-auto space-x-6">
+                <button class="btn btn-primary mx-auto">Demo</button>
+                <button class="btn btn-primary mx-auto">Code</button>
               </div>
             </div>
           </div>
