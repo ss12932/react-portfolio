40adfaba5ad182acb541e03ea1f1175bf49b3bee
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-25 16:17:55 +0100

    fix skills sctn cutting into work sctn @ lower vp

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,7 +14,10 @@ import Heroku from '../assets/heroku.png';
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+    <div
+      name="skills"
+      className="w-full min-h-screen bg-[#0a192f] text-gray-300"
+    >
       {/* container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
