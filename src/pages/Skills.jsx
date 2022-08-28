695a44f489cafc18a434b9f537295b82bf14ce4c
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-28 13:18:29 +0100

    added extra skills + fixed css sizing

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -14,6 +14,10 @@ import Heroku from '../assets/heroku.png';
 import Mongoose from '../assets/mongoose.png';
 import Express from '../assets/express.png';
 import Vercel from '../assets/vercel.png';
+import Postman from '../assets/postman.png';
+import GraphQL from '../assets/graphql.png';
+import Typescript from '../assets/typescript.png';
+import Sequelize from '../assets/sequelize.png';
 
 const Skills = () => {
   return (
@@ -32,72 +36,164 @@ const Skills = () => {
         {/* TODO: sort out attributions to images later */}
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={HTML}
+              alt="HTML icon"
+            />
             <p className="my-4">HTML</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={CSS}
+              alt="CSS icon"
+            />
             <p className="my-4">CSS</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={JS} alt="Javascript icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={JS}
+              alt="Javascript icon"
+            />
             <p className="my-4">JS</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={JQuery} alt="Git icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={JQuery}
+              alt="Git icon"
+            />
             <p className="my-4">jQuery</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={Bootstrap} alt="Git icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Bootstrap}
+              alt="Git icon"
+            />
             <p className="my-4">Bootstrap</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={Mongo} alt="MongoDB icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Mongo}
+              alt="MongoDB icon"
+            />
             <p className="my-4">MongoDB</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={Node} alt="Node icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Node}
+              alt="Node icon"
+            />
             <p className="my-4">Node</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Tailwind}
+              alt="Tailwind icon"
+            />
             <p className="my-4">Tailwind</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={React} alt="React icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={React}
+              alt="React icon"
+            />
             <p className="my-4">React</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={MySQL} alt="MySQL icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={MySQL}
+              alt="MySQL icon"
+            />
             <p className="my-4">MySQL</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={Firebase} alt="Firebase icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Firebase}
+              alt="Firebase icon"
+            />
             <p className="my-4">Firebase</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={Heroku} alt="Heroku icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Heroku}
+              alt="Heroku icon"
+            />
             <p className="my-4">Heroku</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={Git} alt="Heroku icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Git}
+              alt="Heroku icon"
+            />
             <p className="my-4">Git</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
             <img
-              className="w-20 h-20 mx-auto object-contain"
+              className="w-20 h-20 mx-auto object-contain p-2"
               src={Mongoose}
               alt="Heroku icon"
             />
             <p className="my-4">Mongoose</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={Vercel} alt="Heroku icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Vercel}
+              alt="Heroku icon"
+            />
             <p className="my-4">Vercel</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
-            <img className="w-20 mx-auto" src={Express} alt="Heroku icon" />
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Express}
+              alt="Heroku icon"
+            />
             <p className="my-4">Express</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Postman}
+              alt="Heroku icon"
+            />
+            <p className="my-4">Postman</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={GraphQL}
+              alt="Heroku icon"
+            />
+            <p className="my-4">GraphQL</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Typescript}
+              alt="Heroku icon"
+            />
+            <p className="my-4">Typescript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 hover:bg-gray-300 hover:text-gray-700 duration-500 bg-blue-900">
+            <img
+              className="w-20 h-20 mx-auto object-contain p-2"
+              src={Sequelize}
+              alt="Heroku icon"
+            />
+            <p className="my-4">Sequelize</p>
           </div>
         </div>
       </div>
